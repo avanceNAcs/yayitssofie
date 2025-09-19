3b529f66708a10d31c3485aba7bb24aa6c5b5801
Sofia Avancena <avanceNAcs@gmail.com>
2025-09-19 03:17:18 -0400

    Update navBar.js
    updated path navBar.html not navbar.html

--- navBar/navBar.js
+++ navBar/navBar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('navBar/navbar.html')
+    fetch('navBar/navBar.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to load navbar');
