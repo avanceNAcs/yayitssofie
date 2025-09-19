6162cb1b2399303b8e7b8c113899b635570cc322
Sofia Avancena <avanceNAcs@gmail.com>
2025-09-19 18:15:56 -0400

    Update navBar.js

--- navBar/navBar.js
+++ navBar/navBar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('navBar/navBar.html')
+    fetch('/yayitssofie/navBar/navBar.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to load navbar');
